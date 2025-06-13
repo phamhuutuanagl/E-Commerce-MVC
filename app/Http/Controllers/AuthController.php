3d894ec6ca9831086b12c0f7bf3b0a027a828c60
AGL_Pham_Huu_Tuan <phamhuutuan.agl@gmail.com>
2025-06-13 18:05:53 +0700

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login()
+    {
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function () {
+
+    // Login
+    Route::prefix('login')
+        ->name('login.')
+        ->group(function () {
+            Route::get('/', 'index')->name('get');
+            Route::post('/', 'login')->name('post');
+        });
+
+    
 });
